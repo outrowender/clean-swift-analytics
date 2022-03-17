7201ac7e2097619ed5632106ef8165679658cf00
Wender Patrick <outrowender@gmail.com>
2022-03-17 12:34:51 -0300

    Initial Commit

---
+++ AnalyticsCleanArchitecture/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AnalyticsCleanArchitecture
+//
+//  Created by Wender Patrick on 17/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
